b7f86489f7d76e507613ef43686c1fbd3ba6c167
ajackchan <18268175325>
2024-09-13 20:46:55 -0400

    Print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,31 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+int pgtblprint(pagetable_t pagetable, int depth) {
+  for(int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+
+    if(pte & PTE_V) {
+      printf("..");
+
+      for(int j = 0; j < depth; j++) {
+        printf(" ..");
+      }
+
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+
+      if((pte & (PTE_R | PTE_W | PTE_X)) == 0) {
+        uint64 child = PTE2PA(pte);
+        pgtblprint((pagetable_t)child, depth + 1);
+      }
+    }
+  }
+
+  return 0;
+}
+
+int vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  return pgtblprint(pagetable, 0);
+}
